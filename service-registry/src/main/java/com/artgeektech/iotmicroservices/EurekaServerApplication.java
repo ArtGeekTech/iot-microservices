6ea9d3d3800c67e1fffb6f06cd6cfccb6cef9248
marcogx <gx239@nyu.edu>
2018-08-17 10:21:24 -0700

    add eureka server

---
+++ service-registry/src/main/java/com/artgeektech/iotmicroservices/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.artgeektech.iotmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by guang on 11:15 PM 8/16/18.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
+
